fcda5246122e4ad7ca22a2f07535053046248b22
AviralM <aviralmehndiratta@gmail.com>
2025-07-25 18:49:45 +0530

    Strapi Deployment on AWS EC2 using Docker, Terraform, and GitHub Actions

---
+++ terraform_task5/main.tf
@@ -0,0 +1,50 @@
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name_prefix = "strapi-sg-aviral"
+  description = "Allow inbound Strapi and SSH"
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "strapi_ec2" {
+  ami                         = data.aws_ami.ubuntu_jammy.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+
+  user_data = templatefile("${path.module}/user-data.sh", {
+    docker_tag = var.docker_image_tag
+  })
+
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+
+  tags = {
+    Name = "EC2-Aviral-Mehndiratta"
+  }
+}
+
