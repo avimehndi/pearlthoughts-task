b3be2d21c5724e98ba3e8923c425d66afe240dae
AviralM <aviralmehndiratta@gmail.com>
2025-07-26 23:59:19 +0530

    Task 7: updated all files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,4 @@
-variable "region" {
-  default = "us-east-2"  
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "key_name" {
-  description = "Name of your existing EC2 Key Pair"
-  default     = "my-key-aviral"
-}
-variable "db_username" {}
-variable "db_password" {}
-
-variable "image_uri" {
-  description = "The full ECR image URI"
-  type        = string
+output "strapi_url" {
+  description = "Public URL of the Strapi application"
+  value       = aws_lb.aviral_strapi_alb.dns_name
 }
