790824e95046c473e34e424daaee78a111b12dd5
AviralM <aviralmehndiratta@gmail.com>
2025-07-26 21:53:10 +0530

    task 7: final update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "strapi_url" {
   description = "Public URL to access Strapi"
-  value       = "http://${aws_lb.aviral_alb.dns_name}"
+  value       = "http://${aws_lb.aviral_alb_new.dns_name}"
 }
 
