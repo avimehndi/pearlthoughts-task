23115c0780bd98ba02cba3bc53e4f24c06385e91
AviralM <aviralmehndiratta@gmail.com>
2025-07-27 07:24:45 +0530

    Updated: Task 7

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "alb_dns_name" {
   description = "Public DNS of ALB to access Strapi app"
-  value       = aws_lb.alb.dns_name
+  value       = aws_lb.aviral_strapi_alb.dns_name
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,29 +18,3 @@ variable "container_image" {
   description = "Docker image to deploy"
   type        = string
 }
-
-# ENV-LIKE VARIABLES for Strapi
-
-variable "app_keys" {
-  description = "APP_KEYS used by Strapi"
-  type        = string
-  sensitive   = true
-}
-
-variable "admin_jwt_secret" {
-  description = "ADMIN_JWT_SECRET for Strapi admin"
-  type        = string
-  sensitive   = true
-}
-
-variable "jwt_secret" {
-  description = "JWT_SECRET for Strapi"
-  type        = string
-  sensitive   = true
-}
-
-variable "api_token_salt" {
-  description = "API_TOKEN_SALT for Strapi"
-  type        = string
-  sensitive   = true
-}
