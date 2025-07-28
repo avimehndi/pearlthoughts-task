b7c9129b794f10834ba11afedac750d7da7d617f
AviralM <aviralmehndiratta@gmail.com>
2025-07-28 19:16:00 +0530

    updated Terraform.yml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,6 +5,7 @@ variable "region" {
 
 variable "ecs_task_execution_role_arn" {
   description = "IAM role ARN for ECS task execution"
+  default = "arn:aws:iam::607700977843:role/ecsTaskExecutionRole"
 }
 
 variable "db_username" {
