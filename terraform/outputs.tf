a486f1af55f93b570f01e0ca258140593e2e608e
AviralM <aviralmehndiratta@gmail.com>
2025-07-30 20:03:54 +0530

    Task 10 files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,10 +3,6 @@ output "alb_dns_name" {
   value       = aws_lb.alb.dns_name
 }
 
-output "cloudwatch_dashboard_url" {
-  description = "URL to the CloudWatch Dashboard for ECS monitoring"
-  value       = "https://${var.region}.console.aws.amazon.com/cloudwatch/home?region=${var.region}#dashboards/dashboard/Strapi-ECS-Dashboard-aviral-t9"
-}
 
 output "ecs_service_name" {
   value = aws_ecs_service.strapi_service.name
